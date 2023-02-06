a0388dac0554b4de477bf47984b480a9fa773218
Rosalinda Jackson <neojackson04@gmail.com>
2023-02-06 19:02:01 +0800

    the fist time to try to push code to github

---
+++ 23_2_6.c
@@ -0,0 +1,15 @@
+/*23.02.06_Output odd numbers from 1 to 100*/
+
+#include<stdio.h>
+int main()
+{
+	int a=0;
+	while(a<=100)
+ {
+	if(a%2==1)
+      printf("%d ",a);
+	 
+    a++;
+ }	
+return 0;
+}
